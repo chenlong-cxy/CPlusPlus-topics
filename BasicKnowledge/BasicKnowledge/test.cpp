8f5bf40d0dd6f6f6131e57726b6783aac5824189
chenlong_cxy <chenlonghyl@163.com>
2021-05-20 17:15:34 +0800

    C++入门基础

---
+++ BasicKnowledge/BasicKnowledge/test.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "hello world" << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//int main()
+//{
+//	std::cout << "hello world" << std::endl;
+//	return 0;
+//}
+
+#include <iostream>
+int main()
+{
+	std::cout << "hello world" << std::endl;
+	return 0;
+}
