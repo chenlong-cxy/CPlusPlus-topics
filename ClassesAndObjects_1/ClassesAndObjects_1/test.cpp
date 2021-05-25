08983363f046b48397cddd1630b0ee99a98fc8d9
chenlong_cxy <chenlonghyl@163.com>
2021-05-25 21:10:18 +0800

    C++类和对象（一）

--- ClassesAndObjects_1/ClassesAndObjects_1/test.cpp
+++ ClassesAndObjects_1/ClassesAndObjects_1/test.cpp
@@ -83,16 +83,127 @@
 //	return 0;
 //}
 
-class Person
+//class Person
+//{
+//public:
+//	//显示基本信息
+//	void ShowInfo()
+//	{
+//		cout << _name << "-" << _sex << "-" << _age << endl;
+//	}
+//public:
+//	char* _name;  //姓名
+//	char* _sex;   //性别
+//	int _age;     //年龄
+//};
+
+
+//#include <iostream>
+//using namespace std;
+//// 类中既有成员变量，又有成员函数
+//class A1 {
+//public:
+//	void f1(){}
+//private:
+//	int _a;
+//};
+//// 类中仅有成员函数
+//class A2 {
+//public:
+//	void f2() {}
+//};
+//// 类中什么都没有---空类
+//class A3
+//{};
+//int main()
+//{
+//	cout << sizeof(class A1) << endl;
+//	cout << sizeof(class A2) << endl;
+//	cout << sizeof(class A3) << endl;
+//	return 0;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//class Date
+//{
+//public:
+//	void Display()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//	void SetDate(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//private:
+//	int _year; // 年
+//	int _month; // 月
+//	int _day; // 日
+//};
+//int main()
+//{
+//	Date d1, d2;//实例化两个日期类
+//	d1.SetDate(2021, 5, 25);//设置d1的日期
+//	d2.SetDate(2021, 5, 26);//设置d2的日期
+//	d1.Display();//打印d1的日期
+//	d2.Display();//打印d2的日期
+//	return 0;
+//}
+//
+//#include <iostream>
+//using namespace std;
+//class Date
+//{
+//public:
+//	void Display(Date* this)
+//	{
+//		cout << this->_year << "-" << this->_month << "-" << this->_day << endl;
+//	}
+//	void SetDate(Date* this, int year, int month, int day)
+//	{
+//		this->_year = year;
+//		this->_month = month;
+//		this->_day = day;
+//	}
+//private:
+//	int _year; // 年
+//	int _month; // 月
+//	int _day; // 日
+//};
+//int main()
+//{
+//	Date d1, d2;//实例化两个日期类
+//	d1.SetDate(&d1, 2021, 5, 25);//设置d1的日期
+//	d2.SetDate(&d2, 2021, 5, 26);//设置d2的日期
+//	d1.Display(&d1);//打印d1的日期
+//	d2.Display(&d2);//打印d2的日期
+//	return 0;
+//}
+
+
+#include <iostream>
+using namespace std;
+class A
 {
 public:
-	//显示基本信息
-	void ShowInfo()
+	void PrintA()
 	{
-		cout << _name << "-" << _sex << "-" << _age << endl;
+		cout << _a << endl;
 	}
-public:
-	char* _name;  //姓名
-	char* _sex;   //性别
-	int _age;     //年龄
+	void Show()
+	{
+		cout << "Show()" << endl;
+	}
+private:
+	int _a;
 };
+int main()
+{
+	A* p = nullptr;  //第一句代码
+	p->PrintA();     //第二句代码
+	p->Show();       //第三句代码
+}
