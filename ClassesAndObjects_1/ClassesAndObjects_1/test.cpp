6fc84c1a882fd597ac26321b25573866ef3fd87b
chenlong_cxy <chenlonghyl@163.com>
2021-05-25 19:02:32 +0800

    类和对象

--- ClassesAndObjects_1/ClassesAndObjects_1/test.cpp
+++ ClassesAndObjects_1/ClassesAndObjects_1/test.cpp
@@ -38,11 +38,61 @@
 //	return 0;
 //}
 
-#include "test.h"
-#include <iostream>
-using namespace std;
-//显示基本信息
-void Person::ShowInfo()
+//#include "test.h"
+//#include <iostream>
+//using namespace std;
+////显示基本信息
+//void Person::ShowInfo()
+//{
+//	cout << _name << "-" << _sex << "-" << _age << endl;
+//}
+//class Person
+//{
+//public:
+//	//显示基本信息
+//	void ShowInfo();
+//private:
+//	char* _name;  //姓名
+//	char* _sex;   //性别
+//	int _age;     //年龄
+//};
+
+
+//#include <iostream>
+//using namespace std;
+//class Person
+//{
+//public:
+//	//显示基本信息
+//	void ShowInfo();
+//private:
+//	char* _name;  //姓名
+//	char* _sex;   //性别
+//	int _age;     //年龄
+//};
+//
+////这里需要指定ShowInfo是属于Person这个类域
+//void Person::ShowInfo()
+//{
+//	cout << _name << "-" << _sex << "-" << _age << endl;
+//}
+//int main()
+//{
+//	Person man;
+//
+//	return 0;
+//}
+
+class Person
 {
-	cout << _name << "-" << _sex << "-" << _age << endl;
-}
+public:
+	//显示基本信息
+	void ShowInfo()
+	{
+		cout << _name << "-" << _sex << "-" << _age << endl;
+	}
+public:
+	char* _name;  //姓名
+	char* _sex;   //性别
+	int _age;     //年龄
+};
