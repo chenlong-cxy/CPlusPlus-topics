465634012acc9af46baa85e7ea66582c2c6ddd3e
chenlong_cxy <chenlonghyl@163.com>
2021-05-30 22:08:59 +0800

    类和对象

--- ClassesAndObjects_2/ClassesAndObjects_2/test.cpp
+++ ClassesAndObjects_2/ClassesAndObjects_2/test.cpp
@@ -33,25 +33,116 @@
 //}
 
 
+//#include <iostream>
+//using namespace std;
+////日期类
+//class Date
+//{
+//public:
+//	//1.无参构造函数
+//	Date()
+//	{
+//		_year = 0;
+//		_month = 1;
+//		_day = 1;
+//	}
+//	//2.带参构造函数
+//	Date(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int main()
+//{
+//	Date d1;  //调用无参的构造函数
+//	Date d();  //error，该代码声明了一个名为d的函数，该函数无参，返回一个日期类的对象
+//	Date d2(2021, 5, 27);  //调用带参的构造函数
+//
+//	return 0;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//class Date
+//{
+//public:
+//	//Date(int year = 0, int month = 1, int day = 1)// 构造函数
+//	//{
+//	//	_year = year;
+//	//	_month = month;
+//	//	_day = day;
+//	//}
+//	void Print()
+//	{
+//		cout << _year << "年" << _month << "月" << _day << "日" << endl;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int main()
+//{
+//	Date d1; // 编译器将调用自动生成的默认构造函数对d1进行初始化
+//	d1.Print();
+//	return 0;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//class Date
+//{
+//public:
+//	Date(int year = 0, int month = 1, int day = 1)// 构造函数
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	~Date()// 析构函数
+//	{}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//int main()
+//{
+//	Date d1;// d1先构造
+//	Date d2;// d2后构造
+//
+//	return 0;
+//}
+
+
 #include <iostream>
 using namespace std;
-//日期类
 class Date
 {
 public:
-	//1.无参构造函数
-	Date()
-	{
-		_year = 0;
-		_month = 1;
-		_day = 1;
-	}
-	//2.带参构造函数
-	Date(int year, int month, int day)
+	Date(int year = 0, int month = 1, int day = 1)// 构造函数
 	{
 		_year = year;
 		_month = month;
 		_day = day;
+	}
+	//Date(const Date& d)
+	//{
+	//	_year = d._year;
+	//	_month = d._month;
+	//	_day = d._day;
+	//}
+	void Print()
+	{
+		cout << _year << "年" << _month << "月" << _day << "日" << endl;
 	}
 private:
 	int _year;
@@ -60,9 +151,9 @@ private:
 };
 int main()
 {
-	Date d1;  //调用无参的构造函数
-	Date d();  //error，该代码声明了一个名为d的函数，该函数无参，返回一个日期类的对象
-	Date d2(2021, 5, 27);  //调用带参的构造函数
-
+	Date d1(2021, 5, 30);
+	Date d2(d1); // 用已存在的对象d1创建对象d2
+	d1.Print();
+	d2.Print();
 	return 0;
 }
