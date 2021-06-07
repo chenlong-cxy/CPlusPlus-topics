51db34f3d607e3243d404722cf2cd7df640b2a9b
chenlong_cxy <chenlonghyl@163.com>
2021-06-07 12:56:28 +0800

    模板初阶

---
+++ Template_primary/Template_primary/test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+//// 交换两个整型
+//void Swapi(int* p1, int* p2)
+//{
+//	int tmp = *p1;
+//	*p1 = *p2;
+//	*p2 = tmp;
+//}
+//// 交换两个双精度浮点型
+//void Swapd(double* p1, double* p2)
+//{
+//	double tmp = *p1;
+//	*p1 = *p2;
+//	*p2 = tmp;
+//}
+
+//// 交换两个整型
+//void Swap(int& x, int& y)
+//{
+//	int tmp = x;
+//	x = y;
+//	y = tmp;
+//}
+//// 交换两个双精度浮点型
+//void Swap(double& x, double& y)
+//{
+//	double tmp = x;
+//	x = y;
+//	y = tmp;
+//}
+
+
+class <class T1, class T2, ..., class Tn>
+template <typename T1,typename T2,...,typename Tn>
