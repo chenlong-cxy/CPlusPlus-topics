f1b9a0140e76de7efcf4b07333b0172756e2e35e
chenlong-xcy <chenlonghyl@163.com>
2021-09-10 11:24:31 +0800

    stack的模拟实现

--- stack_implementation/stack_implement/stack.h
+++ stack_implementation/stack_implement/stack.h
@@ -5,32 +5,39 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-namespace cl
+
+namespace cl //防止命名冲突
 {
 	template<class T, class Container = std::deque<T>>
 	class stack
 	{
 	public:
+		//元素入栈
 		void push(const T& x)
 		{
 			_con.push_back(x);
 		}
+		//元素出栈
 		void pop()
 		{
 			_con.pop_back();
 		}
-		T& top()
+		//获取栈顶元素
+		const T& top()
 		{
 			return _con.back();
 		}
-		size_t size()
+		//获取栈中有效元素个数
+		size_t size() const
 		{
 			return _con.size();
 		}
-		bool empty()
+		//判断栈是否为空
+		bool empty() const
 		{
 			return _con.empty();
 		}
+		//交换两个栈中的数据
 		void swap(stack<T, Container>& st)
 		{
 			_con.swap(st._con);
