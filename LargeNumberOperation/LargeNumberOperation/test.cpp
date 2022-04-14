0efd3039e447909ff402c1f42c0dddf5ee314e90
chenlong-xcy <chenlonghyl@163.com>
2022-04-14 18:35:44 +0800

    大数运算（加减乘除）

---
+++ LargeNumberOperation/LargeNumberOperation/test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//大数加法
+string AddString(string num1, string num2)
+{
+
+}
+
+//大数减法
+string SubString(string num1, string num2)
+{
+
+}
+
+//大数乘法
+string MulString(string num1, string num2)
+{
+
+}
+
+//大数除法
+string DivString(string num1, string num2)
+{
+
+}
+int main()
+{
+
+	return 0;
+}
