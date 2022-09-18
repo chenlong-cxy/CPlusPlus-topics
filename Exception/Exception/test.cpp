62911dd1ab64991673362ddd933f933c6caca836
chenlong-xcy <chenlonghyl@163.com>
2022-09-18 20:04:13 +0800

    C++异常

---
+++ Exception/Exception/test.cpp
@@ -0,0 +1,26 @@
+//try
+//{
+//	//保护的标识代码
+//}
+//catch (ExceptionName e1)
+//{
+//	//catch块
+//}
+//catch (ExceptionName e2)
+//{
+//	//catch块
+//}
+//catch (ExceptionName eN)
+//{
+//	//catch块
+//}
+
+////这里表示这个函数会抛出A/B/C/D中的某种类型的异常
+//void func() throw(A, B, C, D);
+////这里表示这个函数只会抛出bad_alloc的异常
+//void* operator new(std::size_t size) throw(std::bad_alloc);
+////这里表示这个函数不会抛出异常
+//void* operator new(std::size_t size, void* ptr) throw();
+
+
+
