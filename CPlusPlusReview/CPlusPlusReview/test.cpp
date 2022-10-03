79897da9e6ae246936d1e0df3bebc5591a066f2d
chenlong-xcy <chenlonghyl@163.com>
2022-10-03 21:11:36 +0800

    C++复习

--- CPlusPlusReview/CPlusPlusReview/test.cpp
+++ CPlusPlusReview/CPlusPlusReview/test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <thread>
 using namespace std;
 //class Person
 //{
@@ -34,12 +35,74 @@ using namespace std;
 //	delete[] p2;
 //	return 0;
 //}
+//int main()
+//{
+//	string* p1 = (string*)malloc(sizeof(string));
+//	new(p1)string;
+//	//new(p1)string("hello");
+//	cout << *p1 << endl;
+//	free(p1);
+//	return 0;
+//}
+//class Test
+//{
+//public:
+//	Test()
+//	{
+//		cout << "Test()" << endl;
+//	}
+//private:
+//	int _b;
+//};
+//class Person
+//{
+//public:
+//	Person(int age)
+//		:_age(age)
+//	{
+//		cout << "Person(int age)" << endl;
+//	}
+//public:
+//	Test _name;
+//	int _age;
+//};
+//int main()
+//{
+//	Person p(20);
+//	return 0;
+//}
+//void func(int& num)
+//{
+//	num++;
+//}
+//int main()
+//{
+//	int a = 10;
+//	cout << a << endl;
+//	thread t1(func, ref(a));
+//	t1.join();
+//	cout << a << endl;
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 10;
+//	cout << a << endl;
+//	thread t1([&a](){a++; });
+//	t1.join();
+//	cout << a << endl;
+//	return 0;
+//}
+void func(int* num)
+{
+	(*num)++;
+}
 int main()
 {
-	string* p1 = (string*)malloc(sizeof(string));
-	new(p1)string;
-	//new(p1)string("hello");
-	cout << *p1 << endl;
-	free(p1);
+	int a = 10;
+	cout << a << endl;
+	thread t1(func, &a);
+	t1.join();
+	cout << a << endl;
 	return 0;
 }
